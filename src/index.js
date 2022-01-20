eaf98db5fae42df08198d8a39f1b385e9ae9514e
Ihor Mazur <i.mazur@wnb.com.ua>
2022-01-20 21:48:40 +0200

    Adding an auth

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const loggerService = require('./services/LoggerService');
 const router = require('./api/routes');
 const errorHandler = require('./api/middleware/errorHandler');
 const loggerHandler = require('./api/middleware/loggerHandler');
+const { guard } = require('./api/middleware/authorization');
 
 const config = {
   host: configService.host,
@@ -19,6 +20,7 @@ const app = new Koa();
 app.use(bodyParser());
 app.use(errorHandler);
 app.use(loggerHandler);
+app.use(guard());
 
 app.use(router.routes());
 
@@ -26,6 +28,6 @@ app.use(router.allowedMethods());
 
 app.listen(config.port, () => {
   loggerService.info(
-    `App is listening on http://${config.host}:${config.port}`,
+    `App is listening on http://${config.host}:${config.port}`
   );
 });
